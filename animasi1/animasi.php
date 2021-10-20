0313d9c31a893689acb2597d1c9dad0a33431c8e
armieeh <armisaniasinarya@gmail.com>
2021-10-20 10:53:07 +0700

    first commit

---
+++ animasi1/animasi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>SIDEBAR</title>
+    <link rel="stylesheet" href="animasi.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+	<div class="middle">
+		<a class="btn" href="#">
+			<i class="bx bx-plus"></i>
+			<br>
+			<i class="isi saldo">Isi Saldo</i>
+		</a>
+		<a class="btn" href="#">
+			<i class="bx bx-minus"></i>
+			<br>
+			<i>Tarik Saldo</i>
+		</a>
+		<a class="btn" href="#">
+			<i class="bx bx-send"></i>
+			<br>
+			<i>Transfer</i>
+		</a>
+
+	</div>
+
+</body>
+</html>
